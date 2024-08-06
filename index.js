e007c5755f8ad033a4d0e3ef8ccdb55f5e493497
dhananjay <daundkardhananjay1729@gmail.com>
2024-08-06 23:24:51 +0530

    added ball movement

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// console.log("hello");
+document.addEventListener('DOMContentLoaded', ()=>{
+    let pingPongTable = document.getElementById('ping-pong-table')
+    let ball = document.getElementById('ball');
+    let ballX = ball.offsetWidth;
+    let ballY = ball.offsetHeight;
+    ball.style.left = `${ballX}px`;
+    ball.style.top = `${ballY}px`;
+
+    //displacement of a ball in both x & y direction
+    let dx = 2;
+    let dy = 2;
+    setInterval(() => {
+        ballX = ballX + dx;
+        ballY = ballY + dy;
+        ball.style.left = `${ballX}px`;
+        ball.style.top = `${ballY}px`;
+        if(ballX > pingPongTable.offsetWidth-ball.offsetWidth || ballX < 0) dx = -dx;
+        if(ballY > pingPongTable.offsetHeight-ball.offsetHeight || ballY < 0) dy = -dy;
+    }, 1);
+})
