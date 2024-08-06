f1207bbd508c359595ae1ebd98a0e34948cf81cc
dhananjay <daundkardhananjay1729@gmail.com>
2024-08-07 01:04:38 +0530

    added collision logic

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let pingPongTable = document.getElementById('ping-pong-table');
     let paddle = document.getElementById('paddle');
     let ball = document.getElementById('ball');
-    let ballX = ball.offsetWidth;
-    let ballY = ball.offsetHeight;
+    let ballX = paddle.offsetLeft + paddle.offsetWidth;
+    let ballY = 0;
     ball.style.left = `${ballX}px`;
     ball.style.top = `${ballY}px`;
 
@@ -15,18 +15,27 @@ document.addEventListener('DOMContentLoaded', ()=>{
         ballY = ballY + dy;
         ball.style.left = `${ballX}px`;
         ball.style.top = `${ballY}px`;
-        if(ballX > pingPongTable.offsetWidth-ball.offsetWidth || ballX < 0) dx = -dx;
-        if(ballY > pingPongTable.offsetHeight-ball.offsetHeight || ballY < 0) dy = -dy;
-    }, 1);
 
-    
+        //collision logic
+        if(ballX < paddle.offsetLeft + paddle.offsetWidth 
+            && ballY > paddle.offsetTop
+            && ballY + ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight ) {
+                dx = dx*(-1);
+            }
+
+        if(ballX > pingPongTable.offsetWidth-ball.offsetWidth || ballX <= 0) dx = dx*(-1);
+        if(ballY > pingPongTable.offsetHeight-ball.offsetHeight || ballY <= 0) dy = dy*(-1);
+    }, 4);
+
+
     let paddleY = 0;
     let dPy = 5;
     document.addEventListener('keydown',(event)=>{
+        event.preventDefault(); //prevents default behaviour of 'event'
         if(event.keyCode == 38 && paddleY > 0){
             //up keypress
-            paddleY = paddleY - dPy; 
-        }else if(event.keyCode == 40 && paddleY <= pingPongTable.offsetHeight - paddle.offsetHeight){
+            paddleY = paddleY + (-1)*dPy; 
+        }else if(event.keyCode == 40 && paddleY < pingPongTable.offsetHeight - paddle.offsetHeight){
             //down keypress
             paddleY = paddleY + dPy;
         }
